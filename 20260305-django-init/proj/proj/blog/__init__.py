55a439492628ea815bb603c26648fad1b59a6b7f
KameZh <cyberbush720@gmail.com>
2026-03-16 21:14:11 +0200

    some uncommitted

---
+++ 20260305-django-init/proj/proj/blog/__init__.py
@@ -0,0 +1,3 @@
+from .apps import BlogConfig
+
+default_app_config = 'proj.blog.BlogConfig'
---
+++ 20260305-django-init/proj/proj/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    date = models.DateField()
+    content = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
